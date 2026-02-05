60886d7ad98d29a76777f12f44816eb0c47816cd
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-02-05 10:43:55 +0100

    Skapat alla projektfiler och intallerat alla tillägg och inställningar för automatisering med vite och sass

---
+++ laboration.3/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pictures: resolve(__dirname, "sass.html"),
+      }
+    }
+  },
+  plugins: [
+    ViteImageOptimizer({
+
+      webp:{
+        quality: 75
+      }
+
+    })
+  ]
+
+  });
