1a0b809b50f01d3d1b96bc6a4e61488aea4c9a80
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-02-27 16:00:17 +0100

    Lagt in grunden för synliga diagram, med icke-aktuella värden

--- laboration.3/src/main.js
+++ laboration.3/src/main.js
@@ -1,6 +1,8 @@
 
 "use strict";
 
+// HAMBURGERMENY
+
 //Visar meny vid klick av hamburgerikon i tablet och desktop
 //Hämtar element för hamburge-ikonen
 const hamburger = document.querySelector('.hamburger');
@@ -19,12 +21,58 @@ hamburger.addEventListener('click',
   });
 
 
+// ANIMATION 
+
 //Trigger för animation på knapp (animation.html)
 
 //Hämtar id för knapp
 const animatedButton = document.querySelector('.animatedbutton');
 
 //Händelselyssnare. Lyssnar på klick av knapp
+if (animatedButton) {
 animatedButton.addEventListener('click', () => {
   animatedButton.classList.add('clicked');
 });
+}
+
+// DIAGRAM (APEX)
+import ApexCharts from "apexcharts";
+
+//Stapeldiagram
+
+const chartOne = document.querySelector(".barchart");
+
+if (chartOne) {
+
+  let options1 = {
+    chart: {
+      type: 'bar'
+    },
+    series: [{
+      name: 'sales',
+      data: [30,40,35,50,49,60,70,91,125]
+    }],
+    xaxis: {
+      categories: [1991,1992,1993,1994,1995,1996,1997,1998,1999]
+    }
+  };
+
+  let chart = new ApexCharts(chartOne, options1);
+  chart.render();
+}
+
+//Cirkeldiagram
+const chartTwo = document.querySelector(".piechart");
+
+if (chartTwo) {
+  let options = {
+    chart: {
+      type: 'pie',
+    },
+       series: [44, 55, 13, 33],
+    labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
+  }
+
+    let chart = new ApexCharts(chartTwo, options);
+  chart.render();
+}
