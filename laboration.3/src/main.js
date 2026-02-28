218657675d54c29d599d2588c1cdbf4e164944c3
Daniela Giovanella <dannnigiovanella@gmail.com>
2026-02-28 20:44:46 +0100

    Lagt till inbäddad karta med iframe från openstreetmap. Skapat funktion som gör det möjligt att söka position. Position hittas med latidude och langitude

--- laboration.3/src/main.js
+++ laboration.3/src/main.js
@@ -93,28 +93,28 @@ async function getStatistics() {
 
     //RITAR UT DIAGRAM
 
-//Stapeldiagram - kursinformation
-const chartOne = document.querySelector(".barchart");
+    //Stapeldiagram - kursinformation
+    const chartOne = document.querySelector(".barchart");
 
-if (chartOne) {
-  let options = {
-    chart: {
-      type: 'bar',
-      height: 700
-    },
-    //plockar ut värden från objektet i topCourses(map)
-    series: [{
-      name: 'Antal sökande',
-      data: topCourses.map(course => course.applicants)
-    }],
-    xaxis: {
-      categories: topCourses.map(course => course.name)
+    if (chartOne) {
+      let options = {
+        chart: {
+          type: 'bar',
+          height: 700
+        },
+        //plockar ut värden från objektet i topCourses(map)
+        series: [{
+          name: 'Antal sökande',
+          data: topCourses.map(course => course.applicants)
+        }],
+        xaxis: {
+          categories: topCourses.map(course => course.name)
+        }
+      };
+
+      let chart = new ApexCharts(chartOne, options);
+      chart.render();
     }
-  };
-
-  let chart = new ApexCharts(chartOne, options);
-  chart.render();
-}
 
 
     //Cirkeldiagram -  programinformation
@@ -152,3 +152,55 @@ getStatistics();
 // KARTA //
 
 
+//Funktion som hämtar koordinater
+async function searchLocation(location) {
+
+  // Url till Nominatim. Parameter q tillåter fri text. Tar värdet av sökinput. encodeURI tillåter mellanslag och specialtecken
+  const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(location)}&format=json&`;
+
+  try {
+    // Hämtar svar från api
+    const response = await fetch(url);
+    // Returnerar svaret i JSON
+    const locationInfo = await response.json();
+
+
+    /* If-sats som kontrollerar värdena från locationInfo
+    Vid sökning på plats ska första vädet i arrayen användas (då de kan finnas flera) [0]
+    Om det finns träff skapas variabel med latitude och longitud
+    parseFloat gör om textstängarna till "flytande" nummer
+    */
+    if (locationInfo.length > 0) {
+      const latitude = parseFloat(locationInfo[0].lat);
+      const longitude = parseFloat(locationInfo[0].lon);
+
+      console.log("Koordinater:", latitude, longitude);
+
+      //bbox zoomar in på platsen och visar minsta och max lat och long för platsen som ska visas (inzoomad cirkel)
+      const bbox = `${longitude - 0.02},${latitude - 0.02},${longitude + 0.02},${latitude + 0.02}`;
+      //Url som visar kartan i iframe från openstreetmap. Med bbox som inzoomat, och lat och long där markör sitter
+      document.querySelector("#map").src = `https://www.openstreetmap.org/export/embed.html?bbox=${bbox}&marker=${latitude},${longitude}`;
+
+      //Om plats inte kan hittas på kartan (visas för användare)
+    } else {
+      alert("Platsen kunde inte hittas.");
+    }
+    //Om något blir fel vid hämtning av datan
+  } catch (error) {
+    console.error("Fel vid hämtning av plats", error);
+  }
+}
+
+
+
+//Eventlyssnare för vad som händer vid inmatning och sök i formuläret
+document.querySelector("#searchinput").addEventListener("submit", function (showLocation) {
+
+  // Gör så att sidan inte laddas om vid sök
+  showLocation.preventDefault();
+  // Hämtar värdet från inputfältet
+  const location = document.querySelector("#search").value;
+
+  // Kallar på funktionen
+  searchLocation(location);
+});
